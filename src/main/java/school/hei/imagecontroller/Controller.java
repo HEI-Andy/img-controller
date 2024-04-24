6996f3745cb0f9b72576b1ab91a0e4eae5998c04
HEI-Andy <hei.andy.8@gmail.com>
2024-04-24 04:23:21 +0300

    initial commit

---
+++ src/main/java/school/hei/imagecontroller/Controller.java
@@ -0,0 +1,26 @@
+package school.hei.imagecontroller;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.image.BufferedImage;
+
+@RestController
+public class Controller {
+    private Image Img;
+
+    public Controller(Image img) {
+        Img = img;
+    }
+
+    @PostMapping(
+            value= "/image",
+            produces = {MediaType.IMAGE_JPEG_VALUE}
+    )
+    public @ResponseBody byte[] postBody(@RequestBody byte[] file) {
+        BufferedImage bufferedColoredImage = Img.toBufferedImage(file);
+        BufferedImage bufferedBNWImage = Img.toBnW(bufferedColoredImage);
+        return Img.toBytes(bufferedBNWImage);
+    }
+}
